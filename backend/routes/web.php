41303c9e30e8a2d7cbaac52e0797691d2f34cbc6
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-04-29 16:37:32 +0900

    React導入

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 追記
+Route::get('{any}', function () {
+    return view('app');
+})->where('any','.*');
+
