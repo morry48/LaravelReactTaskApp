667e9bb288f738c8df8fc0e00604c328add3b083
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-05-01 10:35:14 +0900

    react-router-dom導入しディレクトリ変更

--- backend/resources/js/pages/Example.js
+++ backend/resources/js/pages/Example.js
@@ -10,7 +10,7 @@ function Example() {
                     <div className="card">
                         <div className="card-header">React導入できたわな？？</div>
 
-                        <div className="card-body">I'm an example component!</div>
+                        <div className="card-body">pages下に移動出来た？？</div>
                         <Button color="secondary" variant="contained">ワイがMaterial-UIのボタンやな？？</Button>
                     </div>
                 </div>
