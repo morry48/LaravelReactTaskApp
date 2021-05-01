9c5798a4e6cc237336b79c593121857d89053bf9
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-05-01 20:54:03 +0900

    Homeコンポーネントを作成しrouteingの設定

--- backend/resources/js/pages/Example.js
+++ backend/resources/js/pages/Example.js
@@ -11,7 +11,7 @@ function Example() {
                         <div className="card-header">React導入できたわな？？</div>
 
                         <div className="card-body">pages下に移動出来た？？</div>
-                        <Button color="secondary" variant="contained">Homeに遷移ボタン</Button>
+                        <Button color="secondary" variant="contained" href={`/`}>Homeに遷移ボタン</Button>
                     </div>
                 </div>
             </div>
--- backend/resources/js/route.js
+++ backend/resources/js/route.js
@@ -5,13 +5,15 @@ import {
     Route,
     Switch,
   } from 'react-router-dom';
-import Example from './pages/Example'
+import Example from './pages/Example';
+import Home from './pages/Home';
 
   function App() {
     return (
         <div>
             <Switch>
                 <Route path='/example' exact component={Example} />
+                <Route path='/' exact component={Home} />
             </Switch>
         </div>
     );
