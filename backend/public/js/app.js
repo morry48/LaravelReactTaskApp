c52b50caf9e59a971927531e51f88b6cc99d58f2
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-05-03 19:18:34 +0900

    ステートpostsを一覧表に表示できるようにデータ整形しrowsの要素とする

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -8531,46 +8531,20 @@ var useStyles = (0,_material_ui_core_styles__WEBPACK_IMPORTED_MODULE_4__.default
 }); //ヘッダーのコンテンツ用の配列定義
 
 var headerList = ['名前', 'タスク内容', '編集', '完了'];
-var rows = [{
-  name: "モーリー",
-  content: "肩トレ",
-  editBtn: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_6__.default, {
-    color: "secondary",
-    variant: "contained",
-    children: "\u7DE8\u96C6"
-  }),
-  deleteBtn: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_6__.default, {
-    color: "primary",
-    variant: "contained",
-    children: "\u5B8C\u4E86"
-  })
-}, {
-  name: "ドンキーコング",
-  content: "バナナ補給",
-  editBtn: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_6__.default, {
-    color: "secondary",
-    variant: "contained",
-    children: "\u7DE8\u96C6"
-  }),
-  deleteBtn: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_6__.default, {
-    color: "primary",
-    variant: "contained",
-    children: "\u5B8C\u4E86"
-  })
-}];
 
 function Home() {
   //定義したスタイルを利用するための設定
-  var classes = useStyles();
+  var classes = useStyles(); //postsの状態を管理する
 
   var _useState = (0,react__WEBPACK_IMPORTED_MODULE_0__.useState)([]),
       _useState2 = _slicedToArray(_useState, 2),
       posts = _useState2[0],
-      setPosts = _useState2[1];
+      setPosts = _useState2[1]; //画面に到着したらgetPostsDataを呼ぶ
+
 
   (0,react__WEBPACK_IMPORTED_MODULE_0__.useEffect)(function () {
     getPostsData();
-  }, []);
+  }, []); //一覧情報を取得しステートpostsにセットする
 
   var getPostsData = function getPostsData() {
     axios__WEBPACK_IMPORTED_MODULE_2___default().get('/api/posts').then(function (response) {
@@ -8578,9 +8552,27 @@ function Home() {
     })["catch"](function () {
       console.log('通信に失敗しました');
     });
-  };
+  }; //空配列として定義する
 
-  console.log(posts);
+
+  var rows = []; //postsの要素ごとにrowsで使える形式に変換する
+
+  posts.map(function (post) {
+    return rows.push({
+      name: post.name,
+      content: post.content,
+      editBtn: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_6__.default, {
+        color: "secondary",
+        variant: "contained",
+        children: "\u7DE8\u96C6"
+      }),
+      deleteBtn: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)(_material_ui_core__WEBPACK_IMPORTED_MODULE_6__.default, {
+        color: "primary",
+        variant: "contained",
+        children: "\u5B8C\u4E86"
+      })
+    });
+  });
   return /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
     className: "container",
     children: /*#__PURE__*/(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_3__.jsx)("div", {
--- backend/resources/js/pages/Home.js
+++ backend/resources/js/pages/Home.js
@@ -14,20 +14,6 @@ const useStyles = makeStyles((theme) => createStyles({
 //ヘッダーのコンテンツ用の配列定義
 const headerList = ['名前', 'タスク内容', '編集', '完了'];
 
-let rows = [
-    {
-        name: "モーリー",
-        content: "肩トレ",
-        editBtn: <Button color="secondary" variant="contained">編集</Button>,
-        deleteBtn: <Button color="primary" variant="contained">完了</Button>,
-    },{
-        name: "ドンキーコング",
-        content: "バナナ補給",
-        editBtn: <Button color="secondary" variant="contained">編集</Button>,
-        deleteBtn: <Button color="primary" variant="contained">完了</Button>,
-    },
-];
-
 function Home() {
     //定義したスタイルを利用するための設定
     const classes = useStyles();
@@ -51,6 +37,18 @@ function Home() {
                 console.log('通信に失敗しました');
             });
     }
+
+    //空配列として定義する
+    let rows = [];
+    //postsの要素ごとにrowsで使える形式に変換する
+    posts.map((post) =>
+        rows.push({
+            name: post.name,
+            content: post.content,
+            editBtn: <Button color="secondary" variant="contained">編集</Button>,
+            deleteBtn: <Button color="primary" variant="contained">完了</Button>,
+        })
+    );
 
     return (
         <div className="container">
