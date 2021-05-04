62d5d5bb05ecff92a7d87ecff35e6815bede7545
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-05-04 15:09:25 +0900

    新規登録のバックエンド実装

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'api'], function(){
     Route::get('posts', 'App\Http\Controllers\Api\PostController@index');
+    Route::post('post/create', 'App\Http\Controllers\Api\PostController@create');
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
